8c8786d629fedaa22f7a9354d7298438c32f9837
chapa3001aditya <132785787+chapa3001aditya@users.noreply.github.com>
2023-12-02 16:09:09 +0530

    Create solution_part1.py

---
+++ 2023/Day 1/solution_part1.py
@@ -0,0 +1,18 @@
+with open(r"C:\Users\hi\OneDrive\Desktop\VITB notes\Python Self study\AOC_2023\day1_input.txt",'r')as text:
+    text = text.readlines()
+    text = list(map(lambda x: x.strip(),text))
+
+calibration_value = list()
+
+for query in text:
+    number = str()
+    for i in query:
+        if i.isdigit():
+            number += i
+    calibration_value.append(number)
+
+
+calibration_value = list(map(lambda x: x if len(x)<= 2 else x[0]+x[-1],calibration_value ))
+calibration_value = list(map(lambda x: int(x[0]+x[0]) if len(x)==1 else int(x),calibration_value))
+            
+print(sum(calibration_value))
