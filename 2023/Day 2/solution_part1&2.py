6b7617ac411e778e95ec29d2e112a45de0f8b7f9
chapa3001aditya <132785787+chapa3001aditya@users.noreply.github.com>
2023-12-02 16:11:45 +0530

    Contains solutionof part 1&2

---
+++ 2023/Day 2/solution_part1&2.py
@@ -0,0 +1,66 @@
+with open(r"C:\Users\hi\OneDrive\Desktop\VITB notes\Python Self study\AOC_2023\day2_input.txt",'r') as text:
+    text = text.readlines()
+    text = list(map(lambda x: x.strip().split(";"),text))
+   
+game_ids = list()
+sumofpow = list()
+for round in text :
+    # print(round)
+    red,green,blue = [],[],[]
+    for i in range(len(round)):
+        if i==0:
+            round_0= round[i].split(':')
+            id = round_0[i].split(" ")[1]
+            round_1 = list(map(lambda x: x.strip(),round_0[1].split(',')))
+            for box in round_1:
+                if box.endswith(' red'):
+                    red.append(int(box.split(" ")[0]))
+                elif box.endswith(' green'):
+                    green.append(int(box.split(" ")[0]))
+                elif box.endswith(' blue'):
+                    blue.append(int(box.split(" ")[0]))
+            
+        else:
+            round_11= round[i].split(',')
+            round_11 = list(map(lambda x: x.strip(),round[i].split(',')))
+            # print(round_11)
+            for box in round_11:
+                if box.endswith(' red'):
+                    red.append(int(box.split(" ")[0]))
+                elif box.endswith(' green'):
+                    green.append(int(box.split(" ")[0]))
+                elif box.endswith(' blue'):
+                    blue.append(int(box.split(" ")[0]))
+    
+    sumofpow.append(max(red)*max(green)*max(blue))     # part 2
+
+    red_stat, green_stat, blue_stat = True,True,True
+
+    for val in red:
+        # print(val)
+        if val <= 12:
+            # print('continued_red')
+            continue   
+        else:
+            red_stat = False
+
+    if red_stat:
+        for val in green:
+            if val <= 13:
+                # print('continued_green')
+                continue       
+            else:
+                green_stat = False
+
+    if red_stat and green_stat:
+        for val in blue:
+            if val <= 14:
+                continue
+            else:
+                blue_stat = False
+
+    if red_stat and green_stat and blue_stat:
+        game_ids.append(int(id))  #for part 1
+        
+print('part 1:',sum(game_ids))
+print('part2:',sum(sumofpow))
